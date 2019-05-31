b9c5aab6843ec51ac413641bf96e37ecb60a8d55
Wang Yun <yun.wang@linecorp.com>
2019-05-31 18:54:35 +0900

    feat: add comment for intro

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -1,29 +1,34 @@
-#include	"unp.h"
+#include "unp.h"
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int					sockfd, n;
-	char				recvline[MAXLINE + 1];
-	struct sockaddr_in	servaddr;
+	int sockfd, n;
+	char recvline[MAXLINE + 1];
+	struct sockaddr_in servaddr;
 
 	if (argc != 2)
 		err_quit("usage: a.out <IPaddress>");
 
-	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	// The socket function creates an Internet (AF_INET)
+	// stream (SOCK_STREAM) socket, which is a fancy name for a TCP socket.
+	// The function returns a small integer descriptor that we can use
+	// to identify the socket in all future function calls
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		err_sys("socket error");
 
+	// set servaddr
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port   = htons(13);	/* daytime server */
+	servaddr.sin_port = htons(13); /* daytime server */
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
 		err_quit("inet_pton error for %s", argv[1]);
 
-	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
+	if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) < 0)
 		err_sys("connect error");
 
-	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
-		recvline[n] = 0;	/* null terminate */
+	while ((n = read(sockfd, recvline, MAXLINE)) > 0)
+	{
+		recvline[n] = 0; /* null terminate */
 		if (fputs(recvline, stdout) == EOF)
 			err_sys("fputs error");
 	}
